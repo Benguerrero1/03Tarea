a140ea0584e8c96a1ca1cf1539011ca966a68def
Benguerrero1 <benjaminguerrero1@gmail.com>
2015-10-08 19:53:54 -0300

    Grafico hecho en P2. Solo falta informe

--- RK4Lorentz.py
+++ RK4Lorentz.py
@@ -33,22 +33,34 @@ def f(t, xyz):
 
 tf = 100.
 N_steps = 10000
-dt = tf/N_steps
+h = tf/N_steps
 x=np.zeros(N_steps+1)
 y=np.zeros(N_steps+1)
 z=np.zeros(N_steps+1)
-xyz0 = [1, 1, 1]
+xyz_inicial = [1, 1, 1]
 t=np.linspace(0, 10, N_steps+1)
 
 n=1
 solver = ode(f)
 solver.set_integrator('dopri5', atol=1E-6, rtol=1E-4)
-solver.set_initial_value(xyz0)
+solver.set_initial_value(xyz_inicial)
 while solver.successful() and solver.t < tf and n<=N_steps:
-    solver.integrate(solver.t+dt)
+    solver.integrate(solver.t+h)
     t[n] = solver.t
     x[n] = solver.y[0]
     y[n] = solver.y[1]
     z[n] = solver.y[2]
     n+=1
 
+fig = plt.figure(1,figsize=(8,6))
+fig.clf()
+
+ax = fig.add_subplot(111, projection='3d')
+ax.plot(x, y, z)
+ax.set_xlabel('x')
+ax.set_ylabel('y')
+ax.set_zlabel('z')
+ax.set_title('Atractor de Lorenz, $(x_0,y_0,z_0) = (1,1,1)$')
+plt.savefig('Lorenz.png')
+
+plt.show()
